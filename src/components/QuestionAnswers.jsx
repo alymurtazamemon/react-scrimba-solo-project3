dbbab816f2bc7ae5f45d27c45141b67f24401607
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-10-23 11:08:04 +0500

    add css styling to QA section

--- src/components/QuestionAnswers.jsx
+++ src/components/QuestionAnswers.jsx
@@ -4,18 +4,58 @@ function QuestionAnswers() {
   const [questionAnswers, setQuestionAnswers] = React.useState([]);
 
   React.useEffect(() => {
+    // console.log("called");
     fetch(
       "https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple"
     )
       .then((res) => res.json())
-      .then((obj) => setQuestionAnswers(obj.results));
+      .then((res) => {
+        setQuestionAnswers(res.results);
+      });
   }, []);
 
+  function shuffleArray(arr) {
+    const shuffled = arr
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+    return shuffled;
+  }
+
+  function decodeHtmlCharCodes(str) {
+    return str.replace(/(&#(\d+);)/g, function (match, capture, charCode) {
+      return String.fromCharCode(charCode);
+    });
+  }
+
   const questionAnswersComponents = questionAnswers.map((obj, index) => (
-    <div key={index}>{obj.category}</div>
+    <div key={index}>
+      <h3 className="question">{decodeHtmlCharCodes(obj.question)}</h3>
+      <div className="options">
+        {shuffleArray([obj.correct_answer, ...obj.incorrect_answers]).map(
+          (value, index) => (
+            <h3
+              key={index}
+              className="option"
+              // style={{ backgroundColor: "#F8BCBC" }}
+              // style={{
+              //   backgroundColor: "#94D7A2",
+              //   fontWeight: 500,
+              //   opacity: 1,
+              // }}
+            >
+              {decodeHtmlCharCodes(value)}
+            </h3>
+          )
+        )}
+      </div>
+      <hr />
+    </div>
   ));
 
-  return <section>{questionAnswersComponents}</section>;
+  return (
+    <section className="questionAnswers">{questionAnswersComponents}</section>
+  );
 }
 
 export default QuestionAnswers;
