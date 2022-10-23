306d5d8fb545792bf5e89f8416cc806f2eee3fa1
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-10-23 08:09:25 +0500

    remove blob elements from intro screen

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-import blob1 from "../assets/blobs1.png";
-import blob2 from "../assets/blobs2.png";
 
 function Intro() {
   return (
     <div className="intro">
-      <img src={blob1} className="blob1"></img>
       <h1>Quizzical</h1>
       <p>A Trivia App</p>
       <button>Start Quiz</button>
-      <img src={blob2} className="blob2"></img>
     </div>
   );
 }
